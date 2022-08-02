5da75abebb3342c26514b0e3790bc7c31de619f0
FarouqAleshawi <eng.farouq001@gmail.com>
2022-08-02 09:26:30 -0700

    Add LandController

--- APSS.Web.Mvc/Controllers/HomeController.cs
+++ APSS.Web.Mvc/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace APSS.Web.Mvc.Controllers
         {
             return View();
         }
-
+        public IActionResult simple_form()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
