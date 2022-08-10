5a24e662753c4ef5b282d2cc962113e62d9fa299
matari <matari@DESKTOP-LMAVEVC>
2022-08-10 19:41:00 +0300

    Add roles

---
+++ APSS.Web.Mvc/Controllers/SeedRoles.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using APSS.Domain.Entities;
+
+namespace APSS.Web.Mvc.Controllers;
+
+public static class SeedRoles
+{
+    public static async Task SeedAsync(RoleManager<IdentityRole> roleManager)
+    {
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Group.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Farmer.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Directorate.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.District.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Village.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Root.ToString()));
+        await roleManager.CreateAsync(new IdentityRole(AccessLevel.Presedint.ToString()));
+    }
+}
