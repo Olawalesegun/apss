1293460b225ba63f91a4796646e50f4e7b7a6b4b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-19 17:54:47 +0300

    Implement Argon2 hashing service

---
+++ APSS.Infrastructure.Services/Argon2CryptoHashService.cs
@@ -0,0 +1,31 @@
+﻿using APSS.Domain.Services;
+
+using Konscious.Security.Cryptography;
+
+namespace APSS.Infrastructure.Services;
+
+public sealed class Argon2CryptoHashService : ICryptoHashService
+{
+    private const int HASH_PARALLELISM_DEGREE = 16;
+    private const int HASH_MEMORY_SIZE = 8192;
+    private const int HASH_ITERATION_COUNT = 48;
+    private const int HASH_BYTE_COUNT = 128;
+
+    /// <inheritdoc/>
+    public Task<byte[]> HashAsync(byte[] plain, byte[] salt)
+    {
+        var argon2 = new Argon2i(plain)
+        {
+            DegreeOfParallelism = HASH_PARALLELISM_DEGREE,
+            MemorySize = HASH_MEMORY_SIZE,
+            Iterations = HASH_ITERATION_COUNT,
+            Salt = salt,
+        };
+
+        return argon2.GetBytesAsync(HASH_BYTE_COUNT);
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> VerifyAsync(byte[] hash, byte[] plain, byte[] salt)
+        => hash.SequenceEqual(await HashAsync(plain, salt));
+}
