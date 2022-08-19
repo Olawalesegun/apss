3ff067094b053894f33d8cfc1319fab7ff83c0f6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-20 02:34:15 +0300

    Use `BasicRandomStringGeneratorService` as a base class

--- APSS.Infrastructure.Services/SecureRandomGeneratorService.cs
+++ APSS.Infrastructure.Services/SecureRandomGeneratorService.cs
@@ -2,21 +2,18 @@
 
 using APSS.Domain.Entities;
 using APSS.Domain.Services;
+using APSS.Domain.Services.Util;
 
 namespace APSS.Infrastructure.Services;
 
-public sealed class SecureRandomGeneratorService : IRandomGeneratorService
+public sealed class SecureRandomGeneratorService : BasicRandomStringGeneratorService
 {
     /// <inheritdoc/>
-    public short NextInt16(short min = short.MinValue, short max = short.MaxValue)
-        => (short)RandomNumberGenerator.GetInt32(min, max);
-
-    /// <inheritdoc/>
-    public int NextInt32(int min = int.MinValue, int max = int.MaxValue)
+    public override int NextInt32(int min = int.MinValue, int max = int.MaxValue)
         => RandomNumberGenerator.GetInt32(min, max);
 
     /// <inheritdoc/>
-    public long NextInt64(long min = long.MinValue, long max = long.MaxValue)
+    public override long NextInt64(long min = long.MinValue, long max = long.MaxValue)
     {
         if (min >= max)
             throw new ArgumentException("max bound cannot be lower or equal to min bound");
@@ -28,19 +25,10 @@ public sealed class SecureRandomGeneratorService : IRandomGeneratorService
     }
 
     /// <inheritdoc/>
-    public float NextFloat32(float min = float.MinValue, float max = float.MaxValue)
-        => (float)checked(NextFloat64(min, max));
+    public override double NextFloat64(double min = double.MinValue, double max = double.MaxValue)
+        => (1.0 / NextInt32(2)) * (max - min) + min;
 
     /// <inheritdoc/>
-    public double NextFloat64(double min = double.MinValue, double max = double.MaxValue)
-    {
-        return (1.0 / NextInt32(2)) * (max - min) + min;
-    }
-
-    /// <inheritdoc/>
-    public bool NextBool() => NextInt32(0, 1) == 1;
-
-    /// <inheritdoc/>
-    public IEnumerable<byte> NextBytes(int length)
+    public override IEnumerable<byte> NextBytes(int length)
         => RandomNumberGenerator.GetBytes(length);
 }
--- APSS.Tests.Utils/SimpleRandomGeneratorService.cs
+++ APSS.Tests.Utils/SimpleRandomGeneratorService.cs
@@ -1,5 +1,34 @@
-﻿namespace APSS.Tests.Utils;
+﻿using APSS.Domain.Services.Util;
 
-public sealed class SimpleRandomGeneratorService
+namespace APSS.Tests.Utils;
+
+public sealed class SimpleRandomGeneratorService : BasicRandomStringGeneratorService
 {
+    #region Fields
+
+    private readonly Random _rnd = new(DateTime.Now.Millisecond);
+
+    #endregion Fields
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public override IEnumerable<byte> NextBytes(int length)
+    {
+        var buffer = new byte[length];
+
+        _rnd.NextBytes(buffer);
+
+        return buffer;
+    }
+
+    /// <inheritdoc/>
+    public override double NextFloat64(double min = double.MinValue, double max = double.MaxValue)
+        => _rnd.NextDouble() * (max - min) + min;
+
+    /// <inheritdoc/>
+    public override long NextInt64(long min = long.MinValue, long max = long.MaxValue)
+        => _rnd.NextInt64(min, max);
+
+    #endregion Public Methods
 }
