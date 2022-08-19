9191f010937a19dd87a388555996cf078a86aacf
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-19 22:57:27 +0300

    Implement Argon2i-based hashing service

---
+++ APSS.Tests.Infrastructure.Services/Argon2iCryptoHashServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+using APSS.Domain.Services;
+using APSS.Infrastructure.Services;
+
+using Xunit;
+
+namespace APSS.Tests.Infrastructure.Services;
+
+public class Argon2iCryptoHashServiceTests
+{
+    private const int SALT_LENGTH = 128;
+
+    private readonly IRandomGeneratorService _rndSvc;
+    private readonly ICryptoHashService _cryptoHashSvc;
+
+    public Argon2iCryptoHashServiceTests()
+    {
+        _rndSvc = SecureRandomGeneratorService.Instance;
+        _cryptoHashSvc = new Argon2iCryptoHashService();
+    }
+
+    [Fact]
+    public async Task BinaryFullCycleFact()
+    {
+        var plainLen = _rndSvc.NextInt32(64, 256);
+        var notPlain = _rndSvc.NextBytes(plainLen).ToArray();
+        var plain = _rndSvc.NextBytes(plainLen).ToArray();
+        var salt = _rndSvc.NextBytes(SALT_LENGTH).ToArray();
+        var notSalt = _rndSvc.NextBytes(SALT_LENGTH).ToArray();
+
+        var hash = await _cryptoHashSvc.HashAsync(plain, salt);
+
+        Assert.Equal(Argon2iCryptoHashService.HASH_BYTE_COUNT, hash.Length);
+
+        Assert.False(await _cryptoHashSvc.VerifyAsync(hash, notPlain, salt));
+        Assert.False(await _cryptoHashSvc.VerifyAsync(hash, plain, notSalt));
+        Assert.True(await _cryptoHashSvc.VerifyAsync(hash, plain, salt));
+    }
+
+    [Fact]
+    public async Task StringFullCycleFact()
+    {
+        var plainLen = _rndSvc.NextInt32(64, 256);
+        var plain = _rndSvc.NextString(plainLen);
+        var salt = _rndSvc.NextString(SALT_LENGTH);
+
+        var binaryHash = await _cryptoHashSvc.HashAsync(
+            Encoding.UTF8.GetBytes(plain),
+            Convert.FromBase64String(salt));
+        var hash = await _cryptoHashSvc.HashAsync(plain, salt);
+
+        Assert.Equal(hash, Convert.ToBase64String(binaryHash));
+
+        Assert.True(await _cryptoHashSvc.VerifyAsync(hash, plain, salt));
+        Assert.True(await _cryptoHashSvc.VerifyAsync(
+            binaryHash,
+            Encoding.UTF8.GetBytes(plain),
+            Convert.FromBase64String(salt)));
+    }
+}
