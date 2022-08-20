d96a20423b361d814ac9e86c51d41d33631d3900
matari <matari@DESKTOP-LMAVEVC>
2022-08-20 13:09:34 +0300

    Modify

--- APSS.Web.Mvc/Controllers/PopulationController.cs
+++ APSS.Web.Mvc/Controllers/PopulationController.cs
@@ -1,4 +1,5 @@
-﻿using APSS.Domain.Services;
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Services;
 using APSS.Web.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +9,43 @@ namespace APSS.Web.Mvc.Controllers
     public class PopulationController : Controller
     {
         private readonly IPopulationService _populationSvc;
+        private UserDto _userDto = new UserDto { Name = "yemen", AccessLevel = AccessLevel.Root, UserStatus = UserStatus.Active, SupervisedBy = null };
+        private List<FamilyDto> families = null!;
+        private List<IndividualDto> individuals = null!;
 
         public PopulationController(IPopulationService populationSvc)
         {
             _populationSvc = populationSvc;
+
+            //families List
+            families = new List<FamilyDto>
+           {
+               new FamilyDto{Id=0,Name="ali",CreatedAt=DateTime.Now, LivingLocation="sana'a",User=_userDto},
+               new FamilyDto{Id=1,Name="ali",CreatedAt=DateTime.Now, LivingLocation="sana'a",User=_userDto},
+               new FamilyDto{Id=2,Name="ali",CreatedAt=DateTime.Now, LivingLocation="sana'a",User=_userDto},
+               new FamilyDto{Id=3,Name="ali",CreatedAt=DateTime.Now, LivingLocation="sana'a",User=_userDto},
+               new FamilyDto{Id=4,Name="ali",CreatedAt=DateTime.Now, LivingLocation="sana'a",User=_userDto}
+           };
+
+            //Individual
+            individuals = new List<IndividualDto>
+            {
+                new IndividualDto{Id=0,Name="ali",Job="programmer",Address="sana",PhonNumber="777777777777",
+                    Sex=IndividualSex.Male,SocialStatus=SocialStatus.Unmarried,NationalId="5453535",
+                    CreatedAt=DateTime.Now,DateOfBirth=DateTime.Today,User=_userDto},
+                new IndividualDto{Id=1,Name="ali",Job="programmer",Address="sana",PhonNumber="777777777777",
+                    Sex=IndividualSex.Male,SocialStatus=SocialStatus.Unmarried,NationalId="5453535",
+                    CreatedAt=DateTime.Now,DateOfBirth=DateTime.Today,User=_userDto},
+                new IndividualDto{Id=2,Name="ali",Job="programmer",Address="sana",PhonNumber="777777777777",
+                    Sex=IndividualSex.Male,SocialStatus=SocialStatus.Unmarried,NationalId="5453535",
+                    CreatedAt=DateTime.Now,DateOfBirth=DateTime.Today,User=_userDto},
+                new IndividualDto{Id=3,Name="ali",Job="programmer",Address="sana",PhonNumber="777777777777",
+                    Sex=IndividualSex.Male,SocialStatus=SocialStatus.Unmarried,NationalId="5453535",
+                    CreatedAt=DateTime.Now,DateOfBirth=DateTime.Today,User=_userDto},
+                new IndividualDto{Id=4,Name="ali",Job="programmer",Address="sana",PhonNumber="777777777777",
+                    Sex=IndividualSex.Male,SocialStatus=SocialStatus.Unmarried,NationalId="5453535",
+                    CreatedAt=DateTime.Now,DateOfBirth=DateTime.Today,User=_userDto},
+            };
         }
 
         public ActionResult Index()
@@ -22,30 +56,30 @@ namespace APSS.Web.Mvc.Controllers
         // GET: FamilyController
         public ActionResult GetFamilies()
         {
-            long id;
-            /*            List<FamilyDto> families = await _populationSvc.GetFamiliesAsync(id);
-            */
-            return View();
+            return View(families.ToList());
         }
 
         public ActionResult FamilyDetails(int id)
         {
-            return View();
+            var family = families.Find(f => f.Id == id);
+
+            return View(family!);
         }
 
         // GET: FamilyController/Details/5
         public ActionResult GetIndividuals()
         {
-            return View();
+            return View(individuals.ToList());
         }
 
         public ActionResult IndividualDetails(int id)
         {
-            return View();
+            var individual = individuals.Find(i => i.Id == id);
+            return View(individual);
         }
 
         // GET: FamilyController/Create
-        public ActionResult Create()
+        public ActionResult AddFamily()
         {
             return View();
         }
@@ -53,58 +87,152 @@ namespace APSS.Web.Mvc.Controllers
         // POST: FamilyController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult AddFamily(FamilyDto family)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var newfamily = new FamilyDto
+                {
+                    Id = 434,
+                    Name = family.Name,
+                    LivingLocation = family.LivingLocation,
+                    User = _userDto,
+                    CreatedAt = DateTime.Now
+                };
+                families.Add(family);
+                RedirectToAction(nameof(GetFamilies), families);
             }
-            catch
+            return View(family!);
+        }
+
+        // GET: PuplationController/Create
+        public ActionResult AddIndividual(int id)
+        {
+            var family = families.Find(f => f.Id == id);
+            var individual = new IndividualDto
             {
-                return View();
+                Family = family!
+            };
+            return View(individual);
+        }
+
+        // POST: FamilyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddIndividual(IndividualDto individual)
+        {
+            if (ModelState.IsValid)
+            {
+                var newIndividual = new IndividualDto
+                {
+                    Id = 53453,
+                    Name = individual.Name,
+                    Address = individual.Address,
+                    Job = individual.Job,
+                    DateOfBirth = individual.DateOfBirth,
+                    Sex = individual.Sex,
+                    SocialStatus = individual.SocialStatus,
+                    Family = individual.Family,
+                    NationalId = individual.NationalId,
+                    PhonNumber = individual.PhonNumber,
+                    CreatedAt = DateTime.Now
+                };
+                individuals.Add(newIndividual);
+                RedirectToAction(nameof(GetIndividuals));
             }
+            return View(individual!);
         }
 
         // GET: FamilyController/Edit/5
-        public ActionResult Update(int id)
+        public ActionResult UpdateFamily(int id)
         {
-            return View();
+            var family = families.Find(f => f.Id == id);
+
+            return View("Editfamily", family);
         }
 
         // POST: FamilyController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Update(int id, IFormCollection collection)
+        public ActionResult UpdateFamily(int id, FamilyDto family)
         {
-            try
+            if (family != null)
             {
-                return RedirectToAction(nameof(Index));
+                var oldfamily = families.Find(f => f.Id == id);
+                if (oldfamily != null)
+                {
+                    oldfamily.Name = family.Name;
+                    oldfamily.LivingLocation = family.LivingLocation;
+                }
+                return RedirectToAction(nameof(GetFamilies));
             }
-            catch
+            return View("Editfamily", family);
+        }
+
+        public ActionResult UpdateIndividual(int id)
+        {
+            var individual = individuals.Find(i => i.Id == id);
+
+            return View("EditIndividual", individual);
+        }
+
+        // POST: FamilyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateIndividual(int id, IndividualDto individual)
+        {
+            if (ModelState.IsValid)
             {
-                return View();
+                var oldIndividual = individuals.Find(f => f.Id == id);
+                if (oldIndividual != null)
+                {
+                    oldIndividual.Name = individual.Name;
+                    oldIndividual.Job = individual.Job;
+                    oldIndividual.Address = individual.Address;
+                    oldIndividual.Family = individual.Family;
+                }
+                return RedirectToAction(nameof(GetFamilies));
             }
+            return View("EditIndividual", individual);
         }
 
         // GET: FamilyController/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult DeleteFamily(int id)
         {
-            return View();
+            return View(nameof(GetFamilies));
         }
 
         // POST: FamilyController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteFamily(int id, FamilyDto familyDto)
         {
-            try
+            var family = families.Find(f => f.Id == id);
+            if (family != null)
             {
-                return RedirectToAction(nameof(Index));
+                families.Remove(family);
+                return RedirectToAction(nameof(GetFamilies));
             }
-            catch
+            return View(nameof(GetFamilies));
+        }
+
+        public ActionResult DeleteIndividual(int id)
+        {
+            return View(nameof(GetIndividuals));
+        }
+
+        // POST: FamilyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteIndividual(int id, IndividualDto individualDto)
+        {
+            var individual = individuals.Find(f => f.Id == id);
+            if (individual != null)
             {
-                return View();
+                individuals.Remove(individual);
+                return RedirectToAction(nameof(GetIndividuals));
             }
+            return View(nameof(GetIndividuals));
         }
     }
 }
