402f7b8453f4d382af173593d9db8e7dc4cdfea9
matari <matari@DESKTOP-LMAVEVC>
2022-08-21 00:24:56 +0300

    Modify

--- APSS.Web.Mvc/Program.cs
+++ APSS.Web.Mvc/Program.cs
@@ -1,29 +1,56 @@
-using APSS.Domain.Services;
 using APSS.Application.App;
 using APSS.Domain.Repositories;
+using APSS.Domain.Services;
+using APSS.Infrastructure.Repositores.EntityFramework;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+#region Services
+
+var svc = builder.Services;
+
+// Database context
+svc.AddDbContext<ApssDbContext>(cfg =>
+    cfg.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])
+);
+
+// Unit-of-work
+svc.AddScoped<IUnitOfWork, ApssUnitOfWork>();
+
+// Services
+svc.AddScoped<ILogsService, DatabaseLogsService>();
+svc.AddScoped<IUsersService, UsersService>();
+svc.AddScoped<IPermissionsService, PermissionsService>();
+svc.AddScoped<IAccountsService, AccountsService>();
+svc.AddScoped<IAnimalService, AnimalService>();
+svc.AddScoped<ILandService, LandService>();
+svc.AddScoped<IPopulationService, PopulationService>();
+svc.AddScoped<ISurveysService, SurveysService>();
+
+#endregion Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Environmen-dependent settings
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHttpsRedirection();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Population}/{action=GetFamilies}/{id?}");
 
 app.Run();
