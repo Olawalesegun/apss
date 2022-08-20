ab21af16bf29176c26d094474b6f1d81f051f3f4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-20 04:50:28 +0300

    Add an entity for refresh tokens

---
+++ APSS.Domain.Entities/RefreshToken.cs
@@ -0,0 +1,47 @@
+﻿namespace APSS.Domain.Entities;
+
+/// <summary>
+/// A class to represent a user refresh token
+/// </summary>
+public sealed class RefreshToken : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the device unique identifier
+    /// </summary>
+    public string UniqueIdentifier { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the last ip address that used the token
+    /// </summary>
+    public string LastIpAddress { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the date and time of the last login
+    /// </summary>
+    public DateTime LastLogin { get; set; }
+
+    /// <summary>
+    /// Gets or sets the device name of the device that uses the token
+    /// </summary>
+    public string? DeviceName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the hostname of the device that uses the token
+    /// </summary>
+    public string? HostName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the token string
+    /// </summary>
+    public string Token { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the expiration date of the token
+    /// </summary>
+    public DateTime ValidUntil { get; set; }
+
+    /// <summary>
+    /// Gets or sets the owner account
+    /// </summary>
+    public Account Owner { get; set; } = null!;
+}
