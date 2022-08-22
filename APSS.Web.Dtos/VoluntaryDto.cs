2255de31524ea9673a7ad1ef4f35722e9c4ea888
matari <matari@DESKTOP-LMAVEVC>
2022-08-23 00:26:32 +0300

    Modify view data and validation messages

--- APSS.Web.Dtos/VoluntaryDto.cs
+++ APSS.Web.Dtos/VoluntaryDto.cs
@@ -1,8 +1,16 @@
-﻿namespace APSS.Web.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APSS.Web.Dtos;
 
 public class VoluntaryDto : BaseAuditbleDto
 {
+    [Display(Name = "إسم التطوع")]
+    [Required(ErrorMessage = "يجب إدخال إسم التطوع")]
     public string Name { get; set; } = null!;
+
+    [Display(Name = "مجال التطوع")]
+    [Required(ErrorMessage = "يجب إدخال مجال التطوع")]
     public string Field { get; set; } = null!;
+
     public IndividualDto NameIndividual { get; set; } = null!;
 }
