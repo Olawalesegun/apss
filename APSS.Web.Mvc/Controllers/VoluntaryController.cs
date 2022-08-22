41d5fdf567515a25bfaa525355001dd1dbfedcb3
matari <matari@DESKTOP-LMAVEVC>
2022-08-23 00:28:58 +0300

    add voluntary controller

---
+++ APSS.Web.Mvc/Controllers/VoluntaryController.cs
@@ -0,0 +1,56 @@
+﻿using APSS.Web.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APSS.Web.Mvc.Controllers
+{
+    public class VoluntaryController : Controller
+    {
+        // GET: VoluntaryController/GetVoluntaries/5
+        public ActionResult GetVoluntaries(int id)
+        {
+            return View();
+        }
+
+        // GET: VoluntaryController/AddVoluntary/5
+        public ActionResult AddVoluntary(int id)
+        {
+            return View();
+        }
+
+        // POST:VoluntaryController/AddVoluntary
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddVoluntary(VoluntaryDto voluntary)
+        {
+            return View(voluntary);
+        }
+
+        //GET:VoluntaryController/UpdateVoluntary/5
+        public ActionResult UpdateVoluntary(int id)
+        {
+            return View("EditVoluntary");
+        }
+
+        // POST: VoluntaryController/UpdateVoluntary/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateVoluntary(int id, VoluntaryDto newvoluntary)
+        {
+            return View("EditVoluntary");
+        }
+
+        // GET: VoluntaryController/DeleteVoluntary/5
+        public ActionResult DeleteVoluntary(int id)
+        {
+            return View();
+        }
+
+        // POST: VoluntaryController/DeleteVoluntary/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteVoluntary(int id, VoluntaryDto voluntaryDto)
+        {
+            return RedirectToAction(nameof(GetVoluntaries));
+        }
+    }
+}
