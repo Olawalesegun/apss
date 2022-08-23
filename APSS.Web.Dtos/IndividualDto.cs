08df661ff0ab3e77fea0de0e720d2d0a3735eb56
matari <matari@DESKTOP-LMAVEVC>
2022-08-23 17:05:37 +0300

    Modify property of dto

--- APSS.Web.Dtos/IndividualDto.cs
+++ APSS.Web.Dtos/IndividualDto.cs
@@ -5,15 +5,43 @@ namespace APSS.Web.Dtos;
 
 public class IndividualDto : BaseAuditbleDto
 {
-    [Display(Name="  إسم الفرد")]
+    [Display(Name = "  إسم الفرد")]
     public string Name { get; set; } = null!;
+
     public string Job { get; set; } = null!;
     public IndividualSex Sex { get; set; }
-    public SocialStatusDto SocialStatus { get; set; } = null!;
-    public string PhonNumber { get; set; } = null!;
-    public string NationalId { get; set; } = null!;
+    public SocialStatusDto? SocialStatus { get; set; }
+    public string? PhonNumber { get; set; }
+    public string? NationalId { get; set; }
     public string Address { get; set; } = null!;
     public UserDto User { get; set; } = null!;
-    public DateTime DateOfBirth { get; set; }
+    public DateTime? DateOfBirth { get; set; }
     public FamilyDto Family { get; set; } = null!;
+
+    public enum SocialStatusDto
+    {
+        [Display(Name = "غير محدد")]
+        Unspecified,
+
+        [Display(Name = "عازب ")]
+        Unmarried,
+
+        [Display(Name = "متزوج")]
+        Married,
+
+        [Display(Name = "مطلق")]
+        Divorced,
+
+        [Display(Name = "أرمل")]
+        Widowed,
+    }
+
+    public enum SexDto
+    {
+        [Display(Name = "ذكر ")]
+        Male,
+
+        [Display(Name = "أنثى")]
+        Female
+    }
 }
