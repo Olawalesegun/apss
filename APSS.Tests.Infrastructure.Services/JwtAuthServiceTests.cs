6ac1e733316acb8f7551841b48bc9b701dce5cb3
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-24 02:54:21 +0300

    Add a basic jwt service tests

---
+++ APSS.Tests.Infrastructure.Services/JwtAuthServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using APSS.Domain.Repositories;
+using APSS.Domain.Services;
+
+namespace APSS.Tests.Infrastructure.Services;
+
+public sealed class JwtAuthServiceTests
+{
+    #region Fields
+
+    private readonly IAuthService _authSvc;
+    private readonly IUnitOfWork _uow;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    public JwtAuthServiceTests(IUnitOfWork uow, IAuthService authSvc)
+    {
+        _uow = uow;
+        _authSvc = authSvc;
+    }
+
+    #endregion Public Constructors
+}
