154c73933ae85ec6f2f3607e41ad6599f9b72c0e
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-24 02:52:01 +0300

    Add a basic auth controller

---
+++ APSS.Web.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APSS.Web.Dtos.Forms;
+
+namespace APSS.Web.Mvc.Controllers;
+
+public class AuthController : Controller
+{
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        return View("SignIn", new SignInForm());
+    }
+
+    [HttpPost("SignIn")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> SignIn([FromForm] SignInForm form)
+    {
+        return View(form);
+    }
+}
