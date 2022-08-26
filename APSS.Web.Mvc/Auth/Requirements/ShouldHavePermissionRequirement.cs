0b622efbee274dbbc07643f07eb804cd60388d05
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:28:42 +0300

    Add a base permission requirement

---
+++ APSS.Web.Mvc/Auth/Requirements/ShouldHavePermissionRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+using APSS.Domain.Entities;
+
+namespace APSS.Web.Mvc.Auth.Requirements;
+
+public abstract class ShouldHavePermissionsRequirement : IAuthorizationRequirement
+{
+    private readonly PermissionType _permissions;
+
+    public ShouldHavePermissionsRequirement(PermissionType permission)
+        => _permissions = permission;
+
+    public PermissionType Permission => _permissions;
+}
