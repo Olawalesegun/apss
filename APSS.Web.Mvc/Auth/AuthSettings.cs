6796aa9d1f603001ba62bb14be86b4ff211274b6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 20:29:30 +0300

    Add options for making cookies persistent and allowing refreshing then

--- APSS.Web.Mvc/Auth/AuthSettings.cs
+++ APSS.Web.Mvc/Auth/AuthSettings.cs
@@ -11,4 +11,14 @@ public sealed class AuthSettings
     /// Gets or sets whether the app should support sliding expiration or not
     /// </summary>
     public bool SlidingExpiration { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets whether cookie should be allowed to be refreshed
+    /// </summary>
+    public bool AllowRefresh { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether issued cookies are persistent
+    /// </summary>
+    public bool PersistentCookies { get; set; }
 }
