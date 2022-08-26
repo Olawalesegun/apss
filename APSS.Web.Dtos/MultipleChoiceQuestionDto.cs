232a91a45e84d29af3c3f80e23f2246230cf96eb
matari <matari@DESKTOP-LMAVEVC>
2022-08-26 23:52:21 +0300

    Add class MultipleChoiceQuestionDto

---
+++ APSS.Web.Dtos/MultipleChoiceQuestionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos;
+
+public class MultipleChoiceQuestionDto : QuestionDto
+{
+    /// <summary>
+    /// Gets or sets the collection of possible answers to this question
+    /// </summary>
+    public ICollection<string> CandidateAnswers { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets whether multiple answers can be selected at a time
+    /// </summary>
+    public bool CanMultiSelect { get; set; } = false;
+}
