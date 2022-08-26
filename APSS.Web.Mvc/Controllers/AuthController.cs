0de58ab29de3f25c68122e3bbfe2aa467927e28e
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 18:52:13 +0300

    Add `AuthController` blueprint

--- APSS.Web.Mvc/Controllers/AuthController.cs
+++ APSS.Web.Mvc/Controllers/AuthController.cs
@@ -1,20 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using APSS.Domain.Services;
 using APSS.Web.Dtos.Forms;
 
 namespace APSS.Web.Mvc.Controllers;
 
 public class AuthController : Controller
 {
+    #region Fields
+
+    private readonly IAuthService _authSvc;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    public AuthController(IAuthService authSvc)
+    {
+        _authSvc = authSvc;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
     [HttpGet]
     public IActionResult SignIn()
-    {
-        return View("SignIn", new SignInForm());
-    }
+        => View("SignIn", new SignInForm());
 
     [HttpPost("SignIn")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> SignIn([FromForm] SignInForm form)
     {
+        if (!ModelState.IsValid)
+            return View(form);
+
+        //var (account, token) = await _authSvc.SignInAsync(long.Parse(form.AccountId), form.Password, null!);
         return View(form);
     }
+
+    #endregion Public Methods
 }
