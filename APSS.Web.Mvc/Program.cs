53f8d2a27f2e621c04eca081d667c6014f01a216
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:30:18 +0300

    Configure authorization

--- APSS.Web.Mvc/Program.cs
+++ APSS.Web.Mvc/Program.cs
@@ -1,13 +1,18 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 using APSS.Application.App;
+using APSS.Domain.Entities;
 using APSS.Domain.Repositories;
 using APSS.Domain.Services;
 using APSS.Infrastructure.Repositores.EntityFramework;
 using APSS.Infrastructure.Services;
 using APSS.Web.Mvc.Auth;
+using APSS.Web.Mvc.Auth.Handlers;
+using APSS.Web.Mvc.Auth.Requirements;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,6 +55,32 @@ svc.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     options.SlidingExpiration = settings.SlidingExpiration;
     options.AccessDeniedPath = "/Forbidden/";
     options.EventsType = typeof(TokenValidationEvent);
+});
+
+// Authorization
+svc.AddSingleton<IAuthorizationHandler, ShouldHaveCreatePermissionRequirementHandler>();
+svc.AddSingleton<IAuthorizationHandler, ShouldHaveReadPermissionRequirementHandler>();
+svc.AddSingleton<IAuthorizationHandler, ShouldHaveUpdatePermissionRequirementHandler>();
+svc.AddSingleton<IAuthorizationHandler, ShouldHaveDeletePermissionRequirementHandler>();
+
+svc.AddAuthorization(options =>
+{
+    var registerPermissionPolicy = (PermissionType name, IAuthorizationRequirement requirement) =>
+    {
+        options.AddPolicy(name.GetPermissionValues().First(), policy =>
+        {
+            policy.AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme);
+            policy.RequireAuthenticatedUser();
+            policy.RequireClaim(ClaimTypes.Role);
+            policy.Requirements.Add(requirement);
+        });
+    };
+
+    // policies
+    registerPermissionPolicy(PermissionType.Create, new ShouldHaveCreatePermissionRequirement());
+    registerPermissionPolicy(PermissionType.Read, new ShouldHaveReadPermissionRequirement());
+    registerPermissionPolicy(PermissionType.Update, new ShouldHaveUpdatePermissionRequirement());
+    registerPermissionPolicy(PermissionType.Delete, new ShouldHaveDeletePermissionRequirement());
 });
 
 builder.Services.AddScoped<TokenValidationEvent>();
