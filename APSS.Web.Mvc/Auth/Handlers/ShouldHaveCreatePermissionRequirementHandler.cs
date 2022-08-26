181c19cf0409b01eada559efd03f006adeb6122b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:36:31 +0300

    Remove redundant requirement handlers

--- APSS.Web.Mvc/Auth/Handlers/ShouldHaveCreatePermissionRequirementHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using APSS.Web.Mvc.Auth.Requirements;
-
-namespace APSS.Web.Mvc.Auth.Handlers;
-
-public sealed class ShouldHaveCreatePermissionRequirementHandler
-    : ShouldHavePermissionsRequirementHandler<ShouldHaveCreatePermissionRequirement>
-{ }
