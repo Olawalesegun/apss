d3d432f28b0a98a1b7e4e4c693aff2cdc3641491
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 19:34:58 +0300

    Add a model to be used as the result of authentication settings parsing

---
+++ APSS.Web.Mvc/Auth/AuthSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace APSS.Web.Mvc.Auth;
+
+public sealed class AuthSettings
+{
+    /// <summary>
+    /// Gets or sets the expiration timespan of cookies
+    /// </summary>
+    public TimeSpan CookieExpiration { get; set; } = TimeSpan.FromMinutes(30);
+
+    /// <summary>
+    /// Gets or sets whether the app should support sliding expiration or not
+    /// </summary>
+    public bool SlidingExpiration { get; set; } = true;
+}
