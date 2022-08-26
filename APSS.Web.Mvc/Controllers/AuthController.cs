b18d14f25b344aef3470dfae68bfdd0e78cdd5ce
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 20:31:33 +0300

    Implement `SignIn` method

--- APSS.Web.Mvc/Controllers/AuthController.cs
+++ APSS.Web.Mvc/Controllers/AuthController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using APSS.Domain.Entities;
 using APSS.Domain.Services;
+using APSS.Infrastructure.Services;
 using APSS.Web.Dtos.Forms;
+using APSS.Web.Mvc.Auth;
+using CustomClaims = APSS.Web.Mvc.Auth.CustomClaims;
 
 namespace APSS.Web.Mvc.Controllers;
 
@@ -8,14 +15,17 @@ public class AuthController : Controller
 {
     #region Fields
 
+    private readonly AuthSettings _settings = new();
     private readonly IAuthService _authSvc;
 
     #endregion Fields
 
     #region Public Constructors
 
-    public AuthController(IAuthService authSvc)
+    public AuthController(IConfigurationService configSvc, IAuthService authSvc)
     {
+        configSvc.Bind(nameof(AuthSettings), _settings);
+
         _authSvc = authSvc;
     }
 
@@ -29,13 +39,46 @@ public class AuthController : Controller
 
     [HttpPost("SignIn")]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> SignIn([FromForm] SignInForm form)
+    public async Task<IActionResult> SignIn([FromForm] SignInForm form, [FromQuery] string? returnUrl)
     {
         if (!ModelState.IsValid)
             return View(form);
 
-        //var (account, token) = await _authSvc.SignInAsync(long.Parse(form.AccountId), form.Password, null!);
-        return View(form);
+        var deviceInfo = new LoginInfo
+        {
+            HostName = HttpContext.Request.Host.Value,
+            UserAgent = HttpContext.Request.Headers.UserAgent,
+            LastIpAddress = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown"
+        };
+
+        var (account, token) = await _authSvc.SignInAsync(long.Parse(form.AccountId), form.Password, deviceInfo);
+
+        var claims = new List<Claim>()
+        {
+            new Claim(CustomClaims.Id, account.Id.ToString(), ClaimValueTypes.Integer64),
+            new Claim(ClaimTypes.NameIdentifier, account.User.Name),
+            new Claim(ClaimTypes.Name, account.HolderName),
+            new Claim(ClaimTypes.Role, account.User.AccessLevel.ToRolesString()),
+            new Claim(CustomClaims.Token, token.Value),
+            new Claim(CustomClaims.Permissions, string.Join(',', account.Permissions.GetPermissionValues())),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var authProperties = new AuthenticationProperties
+        {
+            AllowRefresh = _settings.AllowRefresh,
+            ExpiresUtc = !_settings.PersistentCookies ? DateTimeOffset.UtcNow.Add(_settings.CookieExpiration) : null,
+            IsPersistent = _settings.PersistentCookies,
+            IssuedUtc = DateTimeOffset.UtcNow,
+            RedirectUri = returnUrl,
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties);
+
+        return LocalRedirect("/");
     }
 
     #endregion Public Methods
