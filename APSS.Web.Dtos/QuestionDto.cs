d0b92eebdb94615654cbacbf172ad5c7069bbfeb
matari <matari@DESKTOP-LMAVEVC>
2022-08-26 23:52:51 +0300

    Add class QuestionDto

---
+++ APSS.Web.Dtos/QuestionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace APSS.Web.Dtos
+{
+    public class QuestionDto
+    {
+        public uint Index { get; set; }
+
+        public string Text { get; set; } = null!;
+
+        public bool IsRequired { get; set; } = false;
+
+        public SurveyDto Survey { get; set; } = null!;
+
+        public QuestionTypeDto QuestionType { get; set; }
+    }
+}
