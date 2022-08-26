eb20605fe887a18e65d69f73b5f3b93c46f4e06f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:30:36 +0300

    Add a utility attribute to ease the permissions specification

---
+++ APSS.Web.Mvc/Auth/ApssAuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using APSS.Domain.Entities;
+
+namespace APSS.Web.Mvc.Auth;
+
+public sealed class ApssAuthorizedAttribute : AuthorizeAttribute
+{
+    public ApssAuthorizedAttribute(PermissionType permissions)
+    {
+        Policy = String.Join(',', permissions.GetPermissionValues());
+    }
+}
