6e8578e651aac8d8924484eeae041127cb10bf75
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:28:57 +0300

    Add a base permission requirement handler

---
+++ APSS.Web.Mvc/Auth/Handlers/PermissionRequirementHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using APSS.Domain.Entities;
+using APSS.Web.Mvc.Auth.Requirements;
+
+namespace APSS.Web.Mvc.Auth.Handlers;
+
+public class PermissionRequirementHandler
+{
+}
+
+public abstract class ShouldHavePermissionsRequirementHandler<R>
+    : AuthorizationHandler<R> where R : ShouldHavePermissionsRequirement
+{
+    /// <inheritdoc/>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, R requirement)
+    {
+        if (context.User.Claims.
+                    FirstOrDefault(c => c.Type == CustomClaims.Permissions) is var permissionsStr
+            && permissionsStr is null)
+        {
+            return Task.CompletedTask;
+        }
+
+        var claimedPermissions = permissionsStr.Value.Split(',');
+        var requiredPermissions = requirement.Permission.GetPermissionValues();
+
+        if (requiredPermissions.All(claimedPermissions.Contains))
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
