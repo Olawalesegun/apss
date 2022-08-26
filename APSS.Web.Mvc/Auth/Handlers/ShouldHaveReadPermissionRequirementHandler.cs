5d8909a9e305f86a257e389634384cf3e6c48d88
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-26 21:29:28 +0300

    Add requirement handlers for the available permissions

---
+++ APSS.Web.Mvc/Auth/Handlers/ShouldHaveReadPermissionRequirementHandler.cs
@@ -0,0 +1,7 @@
+﻿using APSS.Web.Mvc.Auth.Requirements;
+
+namespace APSS.Web.Mvc.Auth.Handlers;
+
+public sealed class ShouldHaveReadPermissionRequirementHandler
+    : ShouldHavePermissionsRequirementHandler<ShouldHaveReadPermissionRequirement>
+{ }
