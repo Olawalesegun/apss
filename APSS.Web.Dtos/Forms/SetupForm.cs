76467e9a3cdf4d9a36a3acde316d4ebfe68b3e81
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-28 22:09:12 +0300

    Add a data-transfer object for setting up the system

---
+++ APSS.Web.Dtos/Forms/SetupForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APSS.Web.Dtos.Forms;
+
+public sealed class SetupForm
+{
+    [Required]
+    [DisplayName("Holder name")]
+    public string HolderName { get; set; } = null!;
+
+    [Required]
+    [DisplayName("Root password")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+    public string Password { get; set; } = null!;
+}
