7563e21a73bd78598aa22c294cebe3f5659844b0
matari <matari@DESKTOP-LMAVEVC>
2022-08-28 22:34:37 +0300

    Add class dto for multipleChoicequestionanswer

---
+++ APSS.Web.Dtos/MultipleChoiceQuestionAnswerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos
+{
+    public class MultipleChoiceQuestionAnswerDto : QuestionAnswersDto
+    {
+        public ICollection<MultipleChoiceAnswerItemDto> Answers { get; set; } = new List<MultipleChoiceAnswerItemDto>();
+    }
+}
