796ae395bf76db4e9e2b9de5916a9efd19c8c79f
baggashalkamali <bggashalkmali98@gmail.com>
2022-08-28 01:33:19 -1200

    Add function for validation form and function for show and hide password

--- APSS.Web.Mvc/wwwroot/js/modelFunction.js
+++ APSS.Web.Mvc/wwwroot/js/modelFunction.js
@@ -91,3 +91,36 @@ $(document).ready(function () {
         });
     });
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+/*show hide password*/
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
