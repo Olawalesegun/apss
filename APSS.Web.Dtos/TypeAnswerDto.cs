4762c3764fef517e0c623b7ac68bdac244beed23
matari <matari@DESKTOP-LMAVEVC>
2022-08-28 22:40:32 +0300

    Add class dto for all types answer

---
+++ APSS.Web.Dtos/TypeAnswerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos
+{
+    public class TypeAnswerDto
+    {
+        public TextQuestionAnswerDto? TextQuestionAnswer { get; set; }
+        public LogicalQuestionAnswerDto? LogicalQuestionAnswer { get; set; }
+        public MultipleChoiceQuestionAnswerDto? MultipleChoiceQuestionAnswer { get; set; }
+    }
+}
