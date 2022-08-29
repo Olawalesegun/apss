bb90de31c8919e892899872df32a4eefd3f491b5
matari <matari@DESKTOP-LMAVEVC>
2022-08-29 17:42:49 +0300

    Add validation Message

--- APSS.Web.Dtos/MultipleChoiceAnswerItemDto.cs
+++ APSS.Web.Dtos/MultipleChoiceAnswerItemDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace APSS.Web.Dtos
 {
     public class MultipleChoiceAnswerItemDto : BaseAuditbleDto
     {
+        [Display(Name = "Choice")]
+        [Required(ErrorMessage = "Field Choice is required")]
         public string Value { get; set; } = null!;
     }
 }
