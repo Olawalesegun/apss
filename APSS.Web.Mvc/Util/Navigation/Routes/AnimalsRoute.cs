b0fc5baaad3f307f314e33f0899a8cd835b9e438
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-03 01:32:46 +0300

    Implement main routes

---
+++ APSS.Web.Mvc/Util/Navigation/Routes/AnimalsRoute.cs
@@ -0,0 +1,15 @@
+﻿namespace APSS.Web.Mvc.Util.Navigation.Routes;
+
+public sealed class AnimalsRoute : Route
+{
+    public AnimalsRoute(IRoute parent) : base(parent, "Animals", "Animals", icon: Icon.Cow)
+    {
+        Groups = new Route(this, "Animal Groups", "Groups", icon: Icon.Cow);
+        Products = new Route(this, "Animal Products", "Products", icon: Icon.Cheese);
+        Units = new Route(this, "Animal Product Units", "Units", icon: Icon.Scale);
+    }
+
+    public IRoute Groups { get; init; }
+    public IRoute Products { get; init; }
+    public IRoute Units { get; init; }
+}
---
+++ APSS.Web.Mvc/Util/Navigation/Routes/LandsRoute.cs
@@ -0,0 +1,17 @@
+﻿namespace APSS.Web.Mvc.Util.Navigation.Routes;
+
+public sealed class LandsRoute : Route
+{
+    public LandsRoute(IRoute parent) : base(parent, "Lands", "Lands", icon: Icon.Mountain)
+    {
+        Lands = new Route(this, "Lands", "Lands", icon: Icon.Mountain);
+        Products = new Route(this, "Land Products", "Products", icon: Icon.Seeding);
+        Units = new Route(this, "Land Product Units", "Units", icon: Icon.Ruler);
+    }
+
+    public IRoute Lands { get; init; }
+    public IRoute Products { get; init; }
+    public IRoute Units { get; init; }
+
+    public override IRoute DefaultRoute => Lands;
+}
