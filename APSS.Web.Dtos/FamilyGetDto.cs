5204fc064d8808bd69647929a0c37068ab2fb27a
matari <matari@DESKTOP-LMAVEVC>
2022-09-03 04:18:31 +0300

    Add classDto for GetFamilies view

---
+++ APSS.Web.Dtos/FamilyGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace APSS.Web.Dtos;
+
+public class FamilyGetDto : BaseAuditbleDto
+{
+    public string Name { get; set; } = null!;
+    public string LivingLocation { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+}
