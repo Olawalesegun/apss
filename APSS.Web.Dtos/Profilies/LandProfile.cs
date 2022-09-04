6fe79768db4cf85e6f36dad0044d76d883986a0a
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-04 06:27:51 -0700

    Add land profile

---
+++ APSS.Web.Dtos/Profilies/LandProfile.cs
@@ -0,0 +1,14 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public class LandProfile : Profile
+{
+    public LandProfile()
+    {
+        CreateMap<Land, LandDto>()
+            .ForMember(d => d.OwnedBy, s => s.MapFrom(s => CreateMap<User, UserDto>()))
+            .ReverseMap();
+    }
+}
