a4ae42afe997507b28b4984a412a9b9395567a08
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-04 06:27:06 -0700

    Add land product profile

---
+++ APSS.Web.Dtos/Profilies/LandProductProfile.cs
@@ -0,0 +1,17 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public class LandProductProfile : Profile
+{
+    public LandProductProfile()
+    {
+        CreateMap<LandProduct, LandProductDto>()
+            .ForMember(d => d.AddedBy, f => f.MapFrom(s => CreateMap<User, UserDto>()))
+            .ForMember(d => d.Unit, f => f.MapFrom(f => CreateMap<LandProductUnit, LandProductUnitDto>()))
+            .ForMember(d => d.ProducedIn, f => f.MapFrom(s => CreateMap<Season, SeasonDto>()))
+            .ForMember(d => d.Producer, f => f.MapFrom(s => CreateMap<Land, LandDto>()))
+            .ReverseMap();
+    }
+}
