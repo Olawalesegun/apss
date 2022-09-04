265a816c8b8513e3ccbb8e02a2411008a31b6603
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-04 06:28:21 -0700

    Add season profile

---
+++ APSS.Web.Dtos/Profilies/SeasonProfile.cs
@@ -0,0 +1,13 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public class SeasonProfile : Profile
+{
+    public SeasonProfile()
+    {
+        CreateMap<Season, SeasonDto>()
+            .ReverseMap();
+    }
+}
