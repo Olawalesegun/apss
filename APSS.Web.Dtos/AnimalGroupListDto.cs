2d6f23c6d507c2a378ab8c92d5f8b859f346595a
baggashalkamali <bggashalkmali98@gmail.com>
2022-09-04 04:16:30 -1200

    Add Dto for List AnimalGroup

---
+++ APSS.Web.Dtos/AnimalGroupListDto.cs
@@ -0,0 +1,30 @@
+﻿using APSS.Web.Dtos.ValueTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Web.Dtos
+{
+    public class AnimalGroupListDto : BaseAuditbleDto
+    {
+        [Display(Name = "Type")]
+        [Required(ErrorMessage = "Type is Required")]
+        public string Type { get; set; } = null!;
+
+        [Required(ErrorMessage = "Quantity is Required")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Sex")]
+        public SexDto Sex { get; set; }
+
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Confirm")]
+        public bool Confirm { get; set; }
+    }
+}
