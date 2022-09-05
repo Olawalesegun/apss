f5319387467be9b12c92adb55cad936c0b68faef
baggashalkamali <bggashalkmali98@gmail.com>
2022-09-04 18:23:51 -1200

    Edit AnimalRoute

--- APSS.Web.Mvc/Util/Navigation/Routes/AnimalsRoute.cs
+++ APSS.Web.Mvc/Util/Navigation/Routes/AnimalsRoute.cs
@@ -1,12 +1,14 @@
-﻿namespace APSS.Web.Mvc.Util.Navigation.Routes;
+﻿using APSS.Web.Mvc.Areas.Controllers;
+
+namespace APSS.Web.Mvc.Util.Navigation.Routes;
 
 public sealed class AnimalsRoute : Route
 {
     public AnimalsRoute(IRoute parent) : base(parent, "Animals", "Animals", icon: Icon.Cow)
     {
-        Groups = new Route(this, "Animal Groups", "Groups", icon: Icon.Cow);
-        Products = new Route(this, "Animal Products", "Products", icon: Icon.Cheese);
-        Units = new Route(this, "Animal Product Units", "Units", icon: Icon.Scale);
+        Groups = FromController<Controllers.GroupsController>(icon: Icon.Cow);
+        Products = FromController<AnimalProductsController>(icon: Icon.Cow);
+        Units = FromController<Controllers.AnimalUnitsController>(icon: Icon.Cow);
     }
 
     public IRoute Groups { get; init; }
