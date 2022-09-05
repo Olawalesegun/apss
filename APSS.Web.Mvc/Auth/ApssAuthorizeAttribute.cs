a4e0288e2ce0efa97e2b37f1987c3417f422f831
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-05 23:57:18 +0300

    Add documentation

--- APSS.Web.Mvc/Auth/ApssAuthorizeAttribute.cs
+++ APSS.Web.Mvc/Auth/ApssAuthorizeAttribute.cs
@@ -14,14 +14,18 @@ public class ApssAuthorizedAttribute : AuthorizeAttribute, IAuthorizationFilter
     /// <summary>
     /// Default constructor
     /// </summary>
-    /// <param name="accessLevel">The required access</param>
-    /// <param name="permissions"></param>
+    /// <param name="accessLevel">The required access level</param>
+    /// <param name="permissions">The required permissions</param>
     public ApssAuthorizedAttribute(AccessLevel accessLevel, PermissionType permissions)
     {
         _accessLevel = accessLevel;
         _permissions = permissions;
     }
 
+    /// <summary>
+    /// A constructor use all access levels
+    /// </summary>
+    /// <param name="permissions">The required permissions</param>
     public ApssAuthorizedAttribute(PermissionType permissions)
         : this(AccessLevel.Root | AccessLevel.Presedint | AccessLevel.Governorate |
                AccessLevel.Directorate | AccessLevel.District | AccessLevel.Village |
