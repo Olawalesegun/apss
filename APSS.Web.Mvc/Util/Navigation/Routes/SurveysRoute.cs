f66626ca71185322f1c7ccf95c32668b8a588aad
matari <matari@DESKTOP-LMAVEVC>
2022-09-06 04:44:43 +0300

    Add route Surveys

---
+++ APSS.Web.Mvc/Util/Navigation/Routes/SurveysRoute.cs
@@ -0,0 +1,17 @@
+﻿using APSS.Web.Mvc.Areas.Surveys.Controllers;
+
+namespace APSS.Web.Mvc.Util.Navigation.Routes;
+
+public sealed class SurveysRoute : Route
+{
+    public SurveysRoute(IRoute parent) : base(parent, "Surveys", "Surveys", icon: Icon.Poll)
+    {
+        Surveys = FromController<SurveysController>(icon: Icon.Poll);
+        Entries = FromController<SurveyEntriesController>(icon: Icon.Poll);
+    }
+
+    public IRoute Surveys { get; init; }
+    public IRoute Entries { get; init; }
+
+    public override IRoute DefaultRoute => Surveys;
+}
