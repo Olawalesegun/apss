eff4984c98352dcf3e9dd5c34da5bd0ef927ad5f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-06 03:08:07 +0300

    Use default implementation for child route with identical path

--- APSS.Web.Mvc/Util/Navigation/Routes/LandsRoute.cs
+++ APSS.Web.Mvc/Util/Navigation/Routes/LandsRoute.cs
@@ -6,16 +6,12 @@ public sealed class LandsRoute : Route
 {
     public LandsRoute(IRoute parent) : base(parent, "Lands", "Lands", icon: Icon.Mountain)
     {
-        Lands = FromCrudController<LandsController>(icon: Icon.Mountain);
         Products = FromCrudController<ProductsController>(icon: Icon.Seeding);
         Units = FromCrudController<UnitsController>(icon: Icon.Ruler);
         Seasons = FromCrudController<SeasonsController>(icon: Icon.Calendar);
     }
 
-    public CrudRoute Lands { get; init; }
     public CrudRoute Products { get; init; }
     public CrudRoute Units { get; init; }
     public CrudRoute Seasons { get; init; }
-
-    public override IRoute DefaultRoute => Lands;
 }
