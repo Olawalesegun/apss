1615889175e57bb5d08564984889438796836f7e
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-06 03:03:23 -0700

    Add Expenses profile

---
+++ APSS.Web.Dtos/Profilies/ExpensesProfile.cs
@@ -0,0 +1,14 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public class ExpensesProfile : Profile
+{
+    public ExpensesProfile()
+    {
+        CreateMap<ProductExpense, ProductExpenseDto>()
+            .ForMember(d => d.SpentOn, f => f.MapFrom(s => CreateMap<LandProductDto, LandProduct>()))
+            .ReverseMap();
+    }
+}
