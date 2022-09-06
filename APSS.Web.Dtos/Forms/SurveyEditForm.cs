00406abeabe55fe712c77de843ab8941f63ca333
matari <matari@DESKTOP-LMAVEVC>
2022-09-06 04:15:59 +0300

    add classform for edit survey

---
+++ APSS.Web.Dtos/Forms/SurveyEditForm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APSS.Web.Dtos.Forms;
+
+public class SurveyEditForm
+{
+    [Required]
+    public long Id { get; set; }
+
+    [Display(Name = "Name")]
+    [Required(ErrorMessage = "Field Name is required")]
+    public string Name { get; set; } = null!;
+
+    [Display(Name = "Expiration Date")]
+    [Required(ErrorMessage = "Field Expiration Date is required")]
+    public DateTime ExpirationDate { get; set; }
+
+    [Display(Name = "Active")]
+    public bool IsActive { get; set; }
+}
