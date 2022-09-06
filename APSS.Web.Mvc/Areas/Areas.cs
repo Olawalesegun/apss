cdda088d5339439275a38a7eaf523c0e37a9d2c4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-06 03:04:15 +0300

    Use strong names instead of string values

--- APSS.Web.Mvc/Areas/Areas.cs
+++ APSS.Web.Mvc/Areas/Areas.cs
@@ -5,10 +5,13 @@ public static class Areas
     public const string Auth = nameof(Auth);
     public const string Setup = nameof(Setup);
     public const string Home = nameof(Home);
-    public const string Animals = "Animals";
-    public const string Users = "Users";
+    public const string Animals = nameof(Animals);
+    public const string Lands = nameof(Lands);
+    public const string Population = nameof(Population);
+    public const string Surveys = nameof(Surveys);
+    public const string Users = nameof(Users);
 
-    public static IEnumerable<string> All
+    public static IEnumerable<string> Dashboard
     {
         get
         {
