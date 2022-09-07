5c63b51269547364b057a41986405e1b8f3ac58b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-07 14:44:28 +0300

    Add a builder for paging options

---
+++ APSS.Web.Mvc/Util/Filtering/FilterBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+using APSS.Domain.Entities;
+using APSS.Domain.Repositories;
+
+namespace APSS.Web.Mvc.Util.Filtering;
+
+public class FilterBuilder<TArgs, TModel> where TModel : AuditableEntity
+{
+    private readonly TArgs _args;
+    private IQueryBuilder<TModel> _query;
+
+    public FilterBuilder(TArgs args, IQueryBuilder<TModel> query)
+    {
+        _args = args;
+        _query = query;
+    }
+
+    public FilterBuilder<TArgs, TModel> Filter(Func<TArgs, Expression<Func<TModel, bool>>> expr)
+    {
+        _query = _query.Where(expr(_args));
+
+        return this;
+    }
+
+    public IQueryBuilder<TModel> Build() => _query;
+}
