a9e654eaa70d25d70de53d18f3472f733cb3ae4e
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-07 15:01:10 -0700

    Fix ExpensesController errors

--- APSS.Web.Mvc/Areas/Lands/Controllers/ExpensesController.cs
+++ APSS.Web.Mvc/Areas/Lands/Controllers/ExpensesController.cs
@@ -24,22 +24,12 @@ namespace APSS.Web.Mvc.Areas.Lands.Controllers
 
         public async Task<IActionResult> Index(long Id)
         {
-            try
-            {
-                var expenses = await (
-                    await _landSvc.GetLandProductExpensesAsync(User.GetAccountId(), Id))
-                    .FirstAsync()
-                    .ToAsyncEnumerable()
-                    .ToListAsync();
+            var expenses = await (
+                await _landSvc.GetLandProductExpensesAsync(User.GetAccountId(), Id))
+                .AsAsyncEnumerable()
+                .ToListAsync();
 
-                return View(expenses.Select(_mapper.Map<ProductExpenseDto>));
-            }
-            catch (Exception ex)
-            {
-                TempData["success"] = ex.ToString();
-
-                return NotFound();
-            }
+            return View(expenses.Select(_mapper.Map<ProductExpenseDto>));
         }
 
         [HttpGet]
@@ -57,138 +47,76 @@ namespace APSS.Web.Mvc.Areas.Lands.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         //[ApssAuthorized(AccessLevel.Farmer, PermissionType.Create)]
-        public async Task<ActionResult> Add(ProductExpenseDto productExpense)
+        public async Task<IActionResult> Add(ProductExpenseDto productExpense)
         {
             if (!ModelState.IsValid)
             {
             }
-            try
-            {
-                await _landSvc.AddLandProductExpense(
-                    User.GetAccountId(),
-                    productExpense.ProductId,
-                    productExpense.Type,
-                    productExpense.Price);
+            await _landSvc.AddLandProductExpense(
+                User.GetAccountId(),
+                productExpense.ProductId,
+                productExpense.Type,
+                productExpense.Price);
 
-                return LocalRedirect(Routes.Dashboard.Lands.Products.FullPath);
-            }
-            catch (Exception ex)
-            {
-                return NotFound(ex);
-            }
-        }
+            TempData["success"] = "Expense Added successfully";
 
-        public ActionResult Details(long Id)
-        {
-            var productExpenses = new List<ProductExpenseDto>
-            {
-                new ProductExpenseDto{Type = "type1", Id = 1, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type2", Id = 2, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type3", Id = 3, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type4", Id = 4, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type5", Id = 5, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type6", Id = 6, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type7", Id = 7, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type8", Id = 8, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type9", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type10", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type11", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-            };
-
-            return View(productExpenses.Where(i => i.Id == Id).First());
+            return LocalRedirect(Routes.Dashboard.Lands.Products.FullPath);
         }
 
         // GET: landProductExpense/Update landProductExpense
-        public ActionResult Update(long Id)
+        //[ApssAuthorized(AccessLevel.Farmer, PermissionType.Update)]
+        public async Task<IActionResult> Update(long Id)
         {
-            var productExpenses = new List<ProductExpenseDto>
-            {
-                new ProductExpenseDto{Type = "type1", Id = 1, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type2", Id = 2, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type3", Id = 3, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type4", Id = 4, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type5", Id = 5, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type6", Id = 6, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type7", Id = 7, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type8", Id = 8, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type9", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type10", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type11", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-            };
-
-            return View(productExpenses.Where(i => i.Id == Id).First());
+            return View(_mapper.Map<ProductExpenseDto>(
+                await (await _landSvc.GetLandProductExpenseAsync(User.GetAccountId(), Id)).FirstAsync()));
         }
 
         // POST: landProductExpense/Update landProductExpense
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Update(ProductExpenseDto productExpense)
+        //[ApssAuthorized(AccessLevel.Farmer, PermissionType.Update)]
+        public async Task<IActionResult> Update(ProductExpenseDto productExpense)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            if (!ModelState.IsValid)
+            { }
+
+            await _landSvc.UpdateLandProductExpenseAsync(
+                User.GetAccountId(),
+                productExpense.Id,
+                f =>
+                {
+                    f.Type = productExpense.Type;
+                    f.Price = productExpense.Price;
+                });
+
+            TempData["success"] = "Expense Updated successfully";
+
+            return LocalRedirect(Routes.Dashboard.Lands.Products.FullPath);
         }
 
         // GET: landProductExpense/Delete landProductExpense
-        public ActionResult Delete(long Id)
+        //[ApssAuthorized(AccessLevel.Farmer, PermissionType.Delete)]
+        public async Task<IActionResult> Delete(long Id)
         {
-            var productExpenses = new List<ProductExpenseDto>
-            {
-                new ProductExpenseDto{Type = "type1", Id = 1, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type2", Id = 2, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type3", Id = 3, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type4", Id = 4, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type5", Id = 5, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type6", Id = 6, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type7", Id = 7, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type8", Id = 8, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type9", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type10", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type11", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-            };
-
-            return View(productExpenses.Where(i => i.Id == Id).First());
+            return View(_mapper.Map<ProductExpenseDto>(
+                await (await _landSvc.GetLandProductExpenseAsync(User.GetAccountId(), Id)).FirstAsync()));
         }
 
-        // POST: landProductExpense/Delete landProductExpense
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(ProductExpenseDto productExpense)
+        //[ApssAuthorized(AccessLevel.Farmer, PermissionType.Delete)]
+        public async Task<IActionResult> DeletePost(long id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            await _landSvc.RemoveLandProductExpenseAsync(User.GetAccountId(), id);
+
+            TempData["success"] = "Expense Removed Successfully";
+
+            return LocalRedirect(Routes.Dashboard.Lands.Products.FullPath);
         }
 
         // GET: landProductExpense/Get landProductExpense
-        public ActionResult GetLandProductExpense(long Id)
+        public async Task<IActionResult> GetExpense(long Id)
         {
-            var productExpenses = new List<ProductExpenseDto>
-            {
-                new ProductExpenseDto{Type = "type1", Id = 1, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type2", Id = 2, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type3", Id = 3, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type4", Id = 4, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type5", Id = 5, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type6", Id = 6, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type7", Id = 7, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type8", Id = 8, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type9", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type10", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-                new ProductExpenseDto{Type = "type11", Id = 9, Price = 2134, CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now.AddDays(3)},
-            };
-
-            return View(productExpenses.Where(i => i.Id == Id).First());
+            return View(_mapper.Map<ProductExpenseDto>(
+                await (await _landSvc.GetLandProductExpenseAsync(User.GetAccountId(), Id)).FirstAsync()));
         }
     }
 }
