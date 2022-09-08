85e6dd4bc23ab7760579a1e214d1d5ddf7a9ede0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-08 06:06:18 +0300

    Add a view model for crud controllers

---
+++ APSS.Web.Mvc/Models/CrudViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+using APSS.Web.Dtos.Parameters;
+
+namespace APSS.Web.Mvc.Models;
+
+public sealed class CrudViewModel<T> : IEnumerable<T>
+{
+    private readonly IEnumerable<T> _stream;
+    private readonly FilteringParameters _args;
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="stream"></param>
+    public CrudViewModel(IEnumerable<T> stream, FilteringParameters args)
+    {
+        _stream = stream;
+        _args = args;
+    }
+
+    /// <summary>
+    /// Gets or sets filtering arguments
+    /// </summary>
+    public FilteringParameters Params => _args;
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+        => _stream.GetEnumerator();
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+        => _stream.GetEnumerator();
+}
