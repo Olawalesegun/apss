34116f3eda7a5aad44ae9216e7f992fe9d18ef2f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-08 06:04:31 +0300

    Readd the default route as an indenpendent child route

--- APSS.Web.Mvc/Util/Navigation/Routes/LandsRoute.cs
+++ APSS.Web.Mvc/Util/Navigation/Routes/LandsRoute.cs
@@ -4,14 +4,18 @@ namespace APSS.Web.Mvc.Util.Navigation.Routes;
 
 public sealed class LandsRoute : CrudRoute
 {
-    public LandsRoute(IRoute parent) : base(parent, "Lands", "Lands", icon: Icon.Mountain)
+    public LandsRoute(IRoute parent) : base(parent, "Land Managment", "Lands", icon: Icon.Mountain)
     {
+        Lands = FromCrudController<LandsController>(icon: Icon.Seeding);
         Products = FromCrudController<ProductsController>(icon: Icon.Seeding);
         Units = FromCrudController<UnitsController>(icon: Icon.Ruler);
         Seasons = FromCrudController<SeasonsController>(icon: Icon.Calendar);
     }
 
+    public CrudRoute Lands { get; init; }
     public CrudRoute Products { get; init; }
     public CrudRoute Units { get; init; }
     public CrudRoute Seasons { get; init; }
+
+    public override IRoute DefaultRoute => Lands;
 }
