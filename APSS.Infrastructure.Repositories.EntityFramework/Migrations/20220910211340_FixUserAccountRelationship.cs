e074f475625cf4b12e907baa0ae475ec33cf930d
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-11 00:13:52 +0300

    Recreate database migrations

---
+++ APSS.Infrastructure.Repositories.EntityFramework/Migrations/20220910211340_FixUserAccountRelationship.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APSS.Infrastructure.Repositories.EntityFramework.Migrations
+{
+    public partial class FixUserAccountRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_AddedById",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_AddedById",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AddedById",
+                table: "Accounts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<long>(
+                name: "AddedById",
+                table: "Accounts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AddedById",
+                table: "Accounts",
+                column: "AddedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_AddedById",
+                table: "Accounts",
+                column: "AddedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
