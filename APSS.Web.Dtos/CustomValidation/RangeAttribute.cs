965f2b7f9e735e9f1b35b5c2dab14236a50e1144
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-09 19:21:52 -0700

    Add custome date validation

--- APSS.Web.Dtos/CustomValidation/RangeAttribute.cs
+++ APSS.Web.Dtos/CustomValidation/RangeAttribute.cs
@@ -2,39 +2,6 @@
 
 namespace APSS.Web.Dtos.CustomValidation;
 
-public class RangeAttribute : ValidationAttribute
-{
-    public double MaxValue { get; set; }
-    public double MinValue { get; set; }
-
-    public RangeAttribute(double minVal, double maxVal)
-    {
-        MaxValue = maxVal;
-        MinValue = minVal;
-    }
-
-    public override bool IsValid(object value)
-    {
-        double inputVal;
-        if (value == null)
-            return false;
-
-        if (double.TryParse(value.ToString(), out inputVal))
-        {
-            /*if (inputVal >= MinValue && inputVal <= MaxValue)
-                return (inputVal % 1) == 0;
-            else
-                return false;*/
-            if (inputVal >= MinValue && inputVal <= MaxValue)
-                return true;
-            else
-                return false;
-        }
-
-        return false;
-    }
-}
-
 public class StartDateAttribute : ValidationAttribute
 {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
@@ -48,27 +15,5 @@ public class StartDateAttribute : ValidationAttribute
         {
             return new ValidationResult(ErrorMessage);
         }
-    }
-}
-
-public class MinValueF : ValidationAttribute
-{
-    private int _length;
-
-    public MinValueF(int length)
-    {
-        this._length = length;
-    }
-
-    public override bool IsValid(object? value)
-    {
-        if (value == null)
-            return value == null;
-
-        string temp = value.ToString();
-        if (temp.Length > _length)
-            return true;
-
-        return false;
     }
 }
