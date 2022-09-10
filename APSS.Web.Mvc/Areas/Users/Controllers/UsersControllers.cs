70280ded4029ffd70450b6988dab5d127824a570
baggashalkamali <bggashalkmali98@gmail.com>
2022-09-09 18:22:39 -1200

    edfit user controller

--- APSS.Web.Mvc/Areas/Users/Controllers/UsersControllers.cs
+++ APSS.Web.Mvc/Areas/Users/Controllers/UsersControllers.cs
@@ -39,95 +39,109 @@ public class UsersController : Controller
         return View(new CrudViewModel<UserDto>(ret, args));
     }
 
-    [HttpGet]
-    public IActionResult Add()
-        => View(new AddUserForm());
+        public async Task<IActionResult> AddUser()
+        {
+            var userDto = new UserDto();
+            ViewBag.status = userDto.userStatus;
+            return View(userDto);
+        }
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Add(AddUserForm form)
-    {
-        if (!ModelState.IsValid)
-            return View(form);
-
-        var _ = await _userService.CreateAsync(
-            User.GetAccountId(),
-            form.Name,
-            form.IsActive ? UserStatus.Active : UserStatus.Inactive);
-
-        return LocalRedirect(Routes.Dashboard.Users.Users.FullPath);
-    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddUser(UserDto user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var add = await _userService.CreateAsync(User.GetAccountId(), user.Name);
+                    TempData["Action"] = "Add Erea";
+                    TempData["success"] = $"{user.Name} is addedd successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception)
+            {
+                return LocalRedirect(Routes.Dashboard.Users.FullPath);
+            }
+            return View(user);
+        }
 
     [HttpGet]
     public async Task<IActionResult> Details(int id)
     {
         var user = await (await _userService.GetUserAsync(User.GetAccountId(), id)).FirstAsync();
 
-        var userDto = new UserDto
+        public async Task<IActionResult> UserDetials(int id)
         {
-            Name = user.Name,
-            Id = user.Id,
-            AccessLevel = user.AccessLevel,
-            UserStatus = user.UserStatus,
-            CreatedAt = user.CreatedAt,
-            ModifiedAt = user.ModifiedAt
-        };
-        return View(userDto);
-    }
-
-    public async Task<IActionResult> Delete(long id)
-    {
-        var user = await (await _userService.GetUserAsync(User.GetAccountId(), id)).AsAsyncEnumerable().ToListAsync();
-        if (user == null) return RedirectToAction(nameof(Index));
-        var users = user.FirstOrDefault();
-        var userDto = new UserDto
-        {
-            Name = users!.Name,
-            Id = users.Id,
-            AccessLevel = users.AccessLevel,
-            UserStatus = users.UserStatus,
-            CreatedAt = users.CreatedAt,
-        };
-        return View(userDto);
-    }
-
-    public async Task<IActionResult> ConfirmDeleteUser(long id)
-    {
-        var user = await (await _userService.GetUserAsync(1, id)).AsAsyncEnumerable().ToListAsync();
-        if (user == null) return NotFound();
-        var delete = await _userService.SetUserStatusAsync(1, id, UserStatus.Terminated);
-        if (delete == null) return RedirectToAction(nameof(Index));
-        TempData["Action"] = "Delete Erea";
-        TempData["success"] = "Erea Deleted Successfully";
-        return LocalRedirect(Routes.Dashboard.Users.FullPath);
-    }
-
-    public async Task<IActionResult> Update(long id)
-    {
-        var user = await (await _userService.GetUserAsync(1, id)).AsAsyncEnumerable().ToListAsync();
-        if (user == null) return RedirectToAction(nameof(Index));
-        var users = user.FirstOrDefault();
-        var userDto = new UserDto
-        {
-            Name = users!.Name,
-            Id = users.Id,
-            UserStatus = users.UserStatus,
-            CreatedAt = users.CreatedAt,
-        };
-        return View(userDto);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Update(UserDto userDto)
-    {
-        var edit = await _userService.UpdateAsync(1, userDto.Id, p =>
+            var user = await (await _userService.GetUserAsync(User.GetAccountId(), id)).AsAsyncEnumerable().ToListAsync();
+            if (user == null) return NotFound();
+            var users = user.FirstOrDefault();
+            var userDto = new UserDto
             {
-                p.Name = userDto.Name;
-                p.UserStatus = userDto.UserStatus;
-            });
-        TempData["Action"] = "Update Erea";
-        TempData["success"] = "Update Failed!!!";
+                Name = users!.Name,
+                Id = users.Id,
+                AccessLevel = users.AccessLevel,
+                userStatus = users.UserStatus,
+                CreatedAt = users.CreatedAt,
+                ModifiedAt = users.ModifiedAt
+            };
+            return View(userDto);
+        }
+
+        public async Task<IActionResult> DeleteUser(long id)
+        {
+            var user = await (await _userService.GetUserAsync(User.GetAccountId(), id)).AsAsyncEnumerable().ToListAsync();
+            if (user == null) return RedirectToAction(nameof(Index));
+            var users = user.FirstOrDefault();
+            var userDto = new UserDto
+            {
+                Name = users!.Name,
+                Id = users.Id,
+                AccessLevel = users.AccessLevel,
+                userStatus = users.UserStatus,
+                CreatedAt = users.CreatedAt,
+            };
+            return View(userDto);
+        }
+
+        public async Task<IActionResult> ConfirmDeleteUser(long id)
+        {
+            var user = await (await _userService.GetUserAsync(1, id)).AsAsyncEnumerable().ToListAsync();
+            if (user == null) return NotFound();
+            var delete = await _userService.SetUserStatusAsync(1, id, UserStatus.Terminated);
+            if (delete == null) return RedirectToAction(nameof(Index));
+            TempData["Action"] = "Delete Erea";
+            TempData["success"] = "Erea Deleted Successfully";
+            return LocalRedirect(Routes.Dashboard.Users.FullPath);
+        }
+
+        public async Task<IActionResult> EditUser(long id)
+        {
+            var user = await (await _userService.GetUserAsync(1, id)).AsAsyncEnumerable().ToListAsync();
+            if (user == null) return RedirectToAction(nameof(Index));
+            var users = user.FirstOrDefault();
+            var userDto = new UserDto
+            {
+                Name = users!.Name,
+                Id = users.Id,
+                userStatus = users.UserStatus,
+                CreatedAt = users.CreatedAt,
+            };
+            return View(userDto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(UserDto userDto)
+        {
+            var edit = await _userService.UpdateAsync(1, userDto.Id, p =>
+                 {
+                     p.Name = userDto.Name;
+                     p.UserStatus = userDto.userStatus;
+                 });
+            TempData["Action"] = "Update Erea";
+            TempData["success"] = "Update Failed!!!";
 
         return LocalRedirect(Routes.Dashboard.Users.FullPath);
     }
