be05da8f926cccb699a1905c1bc37fb8fa176b59
matari <matari@DESKTOP-LMAVEVC>
2022-09-10 23:45:04 +0300

    Add route And modify names of views

--- APSS.Web.Dtos/Forms/FamilyAddForm.cs
+++ APSS.Web.Dtos/Forms/FamilyAddForm.cs
@@ -4,6 +4,8 @@ namespace APSS.Web.Dtos.Forms;
 
 public class FamilyAddForm
 {
+    public long Id { get; set; }
+
     [Required]
     [Display(Name = "Name")]
     public string Name { get; set; } = null!;
