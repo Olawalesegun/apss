f50cf1e5342013c4ffb8f7ba7028633664e47051
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-09 19:22:40 -0700

    Add Lands area dtos validation

--- APSS.Web.Dtos/LandProductUnitDto.cs
+++ APSS.Web.Dtos/LandProductUnitDto.cs
@@ -1,6 +1,11 @@
-﻿namespace APSS.Web.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APSS.Web.Dtos;
 
 public class LandProductUnitDto : BaseAuditbleDto
 {
+    [Display(Name = "Name")]
+    [Required]
+    [MinLength(3, ErrorMessage = "must be more than 2 digit")]
     public string Name { get; set; } = null!;
 }
