33e9ddb33f1931bbdd9cc239021e89abc56a5c25
baggashalkamali <bggashalkmali98@gmail.com>
2022-09-10 17:44:26 -1200

    Edit animal unit route name in animal route

--- APSS.Web.Mvc/Util/Navigation/Routes/AnimalsRoute.cs
+++ APSS.Web.Mvc/Util/Navigation/Routes/AnimalsRoute.cs
@@ -1,5 +1,4 @@
-﻿using APSS.Web.Mvc.Areas.Controllers;
-
+﻿using APSS.Web.Mvc.Areas.Animals.Controllers;
 using APSS.Web.Mvc.Areas.Controllers;
 
 namespace APSS.Web.Mvc.Util.Navigation.Routes;
@@ -10,14 +9,14 @@ public sealed class AnimalsRoute : Route
     {
         Groups = FromCrudController<GroupsController>(icon: Icon.Cow);
         Products = FromCrudController<ProductsController>(icon: Icon.Cow);
-        AnimalUnits = FromCrudController<AnimalUnitsController>(icon: Icon.Cow);
+        Units = FromCrudController<UnitsController>(icon: Icon.Cow);
         Expense = FromCrudController<ExpensesController>(icon: Icon.Cow);
         Confirmation = FromCrudController<ConfirmationsController>(icon: Icon.People);
     }
 
     public CrudRoute Groups { get; init; }
     public CrudRoute Products { get; init; }
-    public CrudRoute AnimalUnits { get; init; }
+    public CrudRoute Units { get; init; }
     public CrudRoute Expense { get; init; }
     public CrudRoute Confirmation { get; init; }
 
