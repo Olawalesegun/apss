a9c5a36a9ea536055b3878f8dd42c2729101180d
matari <matari@DESKTOP-LMAVEVC>
2022-09-11 03:13:33 +0300

    Add profile for skill and voluntary

---
+++ APSS.Web.Dtos/Profilies/VoluntaryProfile.cs
@@ -0,0 +1,13 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public class VoluntaryProfile : Profile
+{
+    public VoluntaryProfile()
+    {
+        CreateMap<Voluntary, VoluntaryDto>()
+            .ReverseMap();
+    }
+}
