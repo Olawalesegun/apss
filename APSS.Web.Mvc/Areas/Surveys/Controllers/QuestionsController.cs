1a9a4254526a19ac00c125574993506eeec7b756
FarouqAleshawi <eng.farouq001@gmail.com>
2022-09-10 17:13:28 -0700

    Update Surveys views

--- APSS.Web.Mvc/Areas/Surveys/Controllers/QuestionsController.cs
+++ APSS.Web.Mvc/Areas/Surveys/Controllers/QuestionsController.cs
@@ -3,6 +3,7 @@ using APSS.Domain.Services;
 using APSS.Web.Dtos;
 using APSS.Web.Dtos.Forms;
 using APSS.Web.Mvc.Auth;
+using APSS.Web.Mvc.Util.Navigation.Routes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APSS.Web.Mvc.Areas.Surveys.Controllers;
@@ -30,7 +31,7 @@ public class QuestionsController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> AddQuestion([FromForm] QuestionAddForm questionDto)
+    public async Task<IActionResult> Add([FromForm] QuestionAddForm questionDto)
     {
         if (!ModelState.IsValid)
         {
@@ -63,10 +64,10 @@ public class QuestionsController : Controller
                 questionDto.Text, questionDto.IsRequired,
                 questionDto.CanMultiSelect!, questionDto.CandidateAnswers!.ToList());
         }
-        return RedirectToAction(nameof(GetQuestionsSurvey), new { id = questionDto.SurveyId });
+        return View("byUser", questionDto.SurveyId);
     }
 
-    public async Task<IActionResult> GetQuestionsSurvey(long id)
+    public async Task<IActionResult> byUser(long id)
     {
         var questions = await _surveysService.GetQuestionsSurveysAsync(User.GetAccountId(), id);
         List<QuestionDto> questionsdto = new List<QuestionDto>();
@@ -87,23 +88,23 @@ public class QuestionsController : Controller
         return View(questionsdto);
     }
 
-    public IActionResult EditQuestion()
+    public IActionResult Update()
     {
         return View();
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult EditQuestion(IFormCollection form)
+    public IActionResult Update(IFormCollection form)
     {
         return View();
     }
 
     [HttpPost, ActionName("DeleteQuestion")]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteQuestion(long id)
+    public async Task<IActionResult> Delete(long id)
     {
         await _surveysService.Removequestion(User.GetAccountId(), id);
-        return RedirectToAction(nameof(GetQuestionsSurvey));
+        return LocalRedirect(Routes.Dashboard.Surveys.Surveys.byUser.FullPath);
     }
 }
