5c5f7316fea7cbc0093608836a4c05ff18587cc0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-11 03:24:28 +0300

    Add a data-transfer object for updating an account's password

---
+++ APSS.Web.Dtos/Forms/UpdatePassowrdForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APSS.Web.Dtos.Forms;
+
+public sealed class UpdatePassowrdForm
+{
+    [Required]
+    public long Id { get; set; }
+
+    [Required]
+    public string Password { get; set; } = null!;
+}
