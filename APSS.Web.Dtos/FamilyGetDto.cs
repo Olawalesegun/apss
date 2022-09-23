9a8fe7f914d8dc14d8e0484628bdb13d5892aa7c
matari <matari@DESKTOP-LMAVEVC>
2022-09-23 22:46:53 +0300

    Delete Unnissary class dtos

--- APSS.Web.Dtos/FamilyGetDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace APSS.Web.Dtos;
-
-public class FamilyGetDto : BaseAuditbleDto
-{
-    public string Name { get; set; } = null!;
-    public string LivingLocation { get; set; } = null!;
-    public string UserName { get; set; } = null!;
-}
--- APSS.Web.Dtos/FamilyIndividualGetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APSS.Web.Dtos;
-
-public class FamilyIndividualGetDto : BaseAuditbleDto
-{
-    public string NameFamily { get; set; } = null!;
-    public string NameIndividual { get; set; } = null!;
-    public bool IsProvider { get; set; }
-    public bool IsParent { get; set; }
-}
