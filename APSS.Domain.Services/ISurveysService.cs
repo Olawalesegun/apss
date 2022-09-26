03603a1e7ccb1ebdfd5ebf20b43c74047791e6ec
matari <matari@DESKTOP-LMAVEVC>
2022-09-26 23:44:09 +0300

    Add Method for getsurveys

--- APSS.Domain.Services/ISurveysService.cs
+++ APSS.Domain.Services/ISurveysService.cs
@@ -131,6 +131,13 @@ public interface ISurveysService
     Task<IQueryBuilder<Survey>> GetAvailableSurveysAsync(long accountId);
 
     /// <summary>
+    /// Asynchrnously gets  surveys for a specific user
+    /// </summary>
+    /// <param name="accountId">The id of the account to get surveys for</param>
+    /// <returns>A query builder with the matching surveys for the user</returns>
+    Task<IQueryBuilder<Survey>> GetSurveysAsync(long accountId);
+
+    /// <summary>
     /// Asynchronously gets a survey by id
     /// </summary>
     /// <param name="accountId">the id of the account who wants to access the survey</param>
@@ -185,7 +192,7 @@ public interface ISurveysService
     /// <param name="accountId">The id of the account removing the survey</param>
     /// <param name="questionId">The id of the question to remove</param>
     /// <returns></returns>
-    Task Removequestion(long accountId, long questionId);
+    Task RemoveQuestion(long accountId, long questionId);
 
     IQueryBuilder<MultipleChoiceQuestionAnswer> GetItemsAnswer(long accountId, long questionId);
 
