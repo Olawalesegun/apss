801b83b28b72f432bd1c29c06676a4e96fdf0671
matari <matari@DESKTOP-LMAVEVC>
2022-09-26 23:44:54 +0300

    Reimplement profiles

--- APSS.Web.Dtos/Profilies/SkillProfile.cs
+++ APSS.Web.Dtos/Profilies/SkillProfile.cs
@@ -8,6 +8,7 @@ public class SkillProfile : Profile
     public SkillProfile()
     {
         CreateMap<Skill, SkillDto>()
+            .ForMember(s => s.BelongsTo, s => s.MapFrom(s => s.BelongsTo))
             .ReverseMap();
     }
 }
