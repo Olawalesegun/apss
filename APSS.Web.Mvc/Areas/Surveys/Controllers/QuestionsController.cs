b5da1037f85fc6adc950bb8e3187ccf31f8b74f1
matari <matari@DESKTOP-LMAVEVC>
2022-09-30 22:17:19 +0300

    impelement actions with service

--- APSS.Web.Mvc/Areas/Surveys/Controllers/QuestionsController.cs
+++ APSS.Web.Mvc/Areas/Surveys/Controllers/QuestionsController.cs
@@ -1,4 +1,5 @@
-﻿using APSS.Domain.Services;
+﻿using APSS.Domain.Entities;
+using APSS.Domain.Services;
 using APSS.Web.Dtos;
 using APSS.Web.Dtos.Forms;
 using APSS.Web.Dtos.Parameters;
@@ -17,56 +18,6 @@ public class QuestionsController : Controller
     public QuestionsController(ISurveysService surveysService)
     {
         _surveysService = surveysService;
-    }
-
-    [HttpGet]
-    public IActionResult Add(long id)
-    {
-        var questiondto = new QuestionAddForm
-        {
-            SurveyId = id
-        };
-
-        return View(questiondto);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Add([FromForm] QuestionAddForm questionDto)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(questionDto);
-        }
-        if (questionDto.QuestionType == QuestionTypeDto.TextQuestion)
-        {
-            await _surveysService
-                .AddTextQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
-                questionDto.Text, questionDto.IsRequired);
-        }
-        else if (questionDto.QuestionType == QuestionTypeDto.LogicalQuestion)
-        {
-            await _surveysService
-                .AddLogicalQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
-                questionDto.Text, questionDto.IsRequired);
-        }
-        else if (questionDto.QuestionType == QuestionTypeDto.MultipleChoiceQuestion)
-        {
-            var awnsers = questionDto.CandidateAnswers!.ToList();
-            foreach (var awnser in awnsers)
-            {
-                if (awnser == null)
-                {
-                    questionDto.CandidateAnswers!.Remove(awnser!);
-                }
-            }
-            await _surveysService
-                .AddMultipleChoiceQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
-                questionDto.Text, questionDto.IsRequired,
-                questionDto.CanMultiSelect!, questionDto.CandidateAnswers!.ToList());
-        }
-
-        return LocalRedirect($"{Routes.Dashboard.Surveys.Questions}?id=${questionDto.SurveyId}");
     }
 
     [HttpGet]
@@ -92,19 +43,99 @@ public class QuestionsController : Controller
     }
 
     [HttpGet]
-    public IActionResult Update()
+    public IActionResult Add(long id)
     {
-        return View();
+        var questiondto = new QuestionAddForm
+        {
+            SurveyId = id
+        };
+
+        return View(questiondto);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Update(IFormCollection form)
+    public async Task<IActionResult> Add([FromForm] QuestionAddForm questionDto)
     {
-        return View();
+        if (!ModelState.IsValid)
+        {
+            return View(questionDto);
+        }
+
+        if (questionDto.QuestionType == QuestionTypeDto.TextQuestion)
+        {
+            await _surveysService
+                .AddTextQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
+                questionDto.Text, questionDto.IsRequired);
+        }
+        else if (questionDto.QuestionType == QuestionTypeDto.LogicalQuestion)
+        {
+            await _surveysService
+                .AddLogicalQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
+                questionDto.Text, questionDto.IsRequired);
+        }
+        else if (questionDto.QuestionType == QuestionTypeDto.MultipleChoiceQuestion)
+        {
+            var awnsers = questionDto.CandidateAnswers!.ToList();
+            foreach (var awnser in awnsers)
+            {
+                if (awnser == null)
+                {
+                    questionDto.CandidateAnswers!.Remove(awnser!);
+                }
+            }
+
+            await _surveysService
+                .AddMultipleChoiceQuestionAsync(User.GetAccountId(), questionDto.SurveyId,
+                questionDto.Text, questionDto.IsRequired,
+                questionDto.CanMultiSelect!, questionDto.CandidateAnswers!.ToList());
+        }
+        TempData["success"] = "Question Added successfully";
+
+        return LocalRedirect(Routes.Dashboard.Surveys.Questions.FullPath + $"?id={questionDto.SurveyId}");
     }
 
-    [HttpPost, ActionName("DeleteQuestion")]
+    [HttpGet]
+    public async Task<IActionResult> Update(long id)
+    {
+        var question = await _surveysService.GetQuestionAsync(User.GetAccountId(), id);
+        var questionDto = new QuestionEditForm
+        {
+            Id = question.Id,
+            Index = question.Index,
+            Text = question.Text,
+            IsRequired = question.IsRequired,
+            SurveyId = question.Survey.Id,
+            QuestionType =
+                (QuestionTypeDto)Enum.Parse(typeof(QuestionTypeDto),
+                question.GetType().ToString().Split('.').Last(), true),
+        };
+
+        if (question is MultipleChoiceQuestion)
+        {
+            var items = await _surveysService.GetItemsAnswer(User.GetAccountId(), question.Id);
+            questionDto.CandidateAnswers = items;
+        }
+
+        return View(questionDto);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Update([FromForm] QuestionEditForm question)
+    {
+        await _surveysService.UpdateQuestionAsync(User.GetAccountId(), question.Id, q =>
+         {
+             q.Text = question.Text;
+             q.IsRequired = question.IsRequired;
+         });
+
+        TempData["success"] = "Question Updated successfully";
+
+        return LocalRedirect(Routes.Dashboard.Surveys.Questions.FullPath + $"?id={question.SurveyId}");
+    }
+
+    [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Delete(long id)
     {
