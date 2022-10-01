29d4754ac81b54e966d4f84d0730c72483554a2e
matari <matari@DESKTOP-LMAVEVC>
2022-10-02 00:35:49 +0300

    Add profile for survey entry

---
+++ APSS.Web.Dtos/Profilies/SurveyEntryProfile.cs
@@ -0,0 +1,15 @@
+﻿using APSS.Domain.Entities;
+using AutoMapper;
+
+namespace APSS.Web.Dtos.Profilies;
+
+public sealed class SurveyEntryProfile : Profile
+{
+    public SurveyEntryProfile()
+    {
+        CreateMap<SurveyEntryDto, SurveyEntry>()
+            .ForMember(e => e.Survey, e => e.MapFrom(e => e.Survey))
+            .ForMember(e => e.MadeBy, e => e.MapFrom(e => e.MadeBy))
+            .ReverseMap();
+    }
+}
